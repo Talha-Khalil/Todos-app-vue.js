e03a99f493a1e148db60eed0934b496d2f74baac
Talha <ka889076@gmail.com>
2023-07-06 17:13:26 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HeaderComponent from './components/HeaderComponent.vue'
+</script>
+
+<template>
+  <HeaderComponent />
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap');
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <header>
+            <nav class="container">
+                <div class="logo">
+                    <img src="../assets/header-logo.png" alt="logo">
+                    <h1>Vue Todos</h1>
+                </div>
+                <ul class="nav_routes">
+                    <RouterLink to="/">Home</RouterLink>
+                    <RouterLink to="/about">About</RouterLink>
+                </ul>
+            </nav>
+        </header>
+
+    </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: #f1f1f1;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 16px;
+
+    .logo {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      img {
+        max-width: 50px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+    }
+
+    .nav_routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
+
+</style>
